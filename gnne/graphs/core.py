e1bbd5127e7698f37625d8f712f3731b25c4d7c2
Nelson_Zhao <dutzhaoyeyu@163.com>
2020-04-08 01:22:08 +0800

    fixed: modify directory

---
+++ gnne/graphs/core.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+
+class Graph2Vec(object):
+
+    def __init__(self):
+        pass
+
+
+    def get_node_embeddings(self):
+        pass
+
+
+    def get_edge_embeddings(self):
+        pass
